5129deeffcc6eaa894b0d5902d9e68260281eb1f
jussipu <25924791+jussipu@users.noreply.github.com>
2020-05-23 16:58:59 +0300

    test

--- ardumower/src/robot.cpp
+++ ardumower/src/robot.cpp
@@ -2145,6 +2145,8 @@ void Robot::setup()
   if (RaspberryPIUse)
     MyRpi.init();
 
+  // TEST BRANCH!!!
+
   //setDefaultTime();
   //init of timer for factory setting
   for (int i = 0; i < MAX_TIMERS; i++)
